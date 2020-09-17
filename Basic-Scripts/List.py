10e83275d0fcfba6cd680ed3f6e66f24faabd5c5
Ajay Neman <66777979+AjayNeman45@users.noreply.github.com>
2020-09-17 23:39:42 +0530

    added some new list method and also added output using comments.

--- Basic-Scripts/List.py
+++ Basic-Scripts/List.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 spam = ['ankit','arjun','arun']
-print(spam)
+print(spam) # output : ['ankit','arjun','arun']
 spam.append('anurag')
-print(spam)
+print(spam) # append element at the last position of the array. output : ['ankit','arjun','arun','anurag']
 spam.insert(1,'elliot')
-print(spam)
+print(spam) # output : ['ankit','elliot','arjun','arun,'anurag']
+spam.pop(1)
+print(spam) # removes the element from the array given at that position of index. output : ['ankit','arjun','arun','anurag']
 spam.remove('arun')
-print(spam)
+print(spam) # output : ['ankit','elliot','arjun','anurag']
+print(spam.index('anurag')) # return the index of the first matched item. output : 3
+spam.reverse()
+print(spam) # reverse the element of the array. output : ['anurag','arjun','elliot','ankit']
+spam.clear()
+print(spam) # Removes all the element the array. output : []
